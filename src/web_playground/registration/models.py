71ece087869af58785d75eecb036f1b38ed7f938
edison0019 <edison_0019@hotmail.com>
2020-07-23 23:22:31 -0400

    creating unit testing in django and optimazing the storage of images in django

--- src/web_playground/registration/models.py
+++ src/web_playground/registration/models.py
@@ -3,16 +3,22 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+#custom function to delete the current picture before using another one
+def delete_old_picture(instance,filename):
+    old_instance = Profile.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return 'profile/' + filename
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models .CASCADE)
-    avatar = models.ImageField(upload_to='profile',null=True,blank=True)
+    avatar = models.ImageField(upload_to=delete_old_picture,null=True,blank=True)
     bio = models.TextField(null=True,blank=True)
     link = models.URLField(null=True,blank=True,max_length=200)
     def __str__(self):
         return str(self.user)
 
-receiver(post_save,sender=User)
+@receiver(post_save,sender=User)
 def profile_creation_signal(sender,instance, **kwargs):
     if kwargs.get('created',False): #this to make sure that the action is executed the first time the instance is created
         Profile.objects.get_or_create(user=instance)
--- src/web_playground/registration/tests.py
+++ src/web_playground/registration/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user('test','tssef@test.com','test134232&@')
+
+    def test_registration_signal(self):
+        exists = Profile.objects.filter(user__username='test').exists()
+        self.assertEqual(exists,True)
